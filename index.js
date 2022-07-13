dd161f5c1fb9245673667179f72b50120ddf1d80
Mark Viti <+>
2022-07-13 10:56:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", 
+    function change_text() {
+        var text = document.getElementById("text");
+        text.innerHTML= "This is really cool!";
+  });
+
